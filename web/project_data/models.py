5ae64684f92d93e6e8c0eb544174ad7ace224218
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-26 15:17:40 -0700

    fixed web/project_data/models.py

--- web/project_data/models.py
+++ web/project_data/models.py
@@ -15,6 +15,10 @@ class Project(models.Model):
     pledged = models.FloatField()
     state = models.CharField(max_length=1024)
     backers = models.IntegerField()
+    country = models.CharField(max_length=1024)
+    usd_pledged = models.FloatField()
+    usd_pledged_real = models.FloatField()
+    usd_goal_real = models.FloatField()
 
     def __str__(self):
         return '{}'.format(self.title)
